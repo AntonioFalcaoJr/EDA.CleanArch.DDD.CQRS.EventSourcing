30906e4fb42782752e666f142b4da1a36ffa0829
Antônio Falcão Jr <arfj@edu.univali.br>
2024-07-13 13:27:55 -0300

    Evolving GuidIdentity design

--- src/Services/Shopping/Command/Domain/Abstractions/Identities/GuidIdentity.cs
+++ src/Services/Shopping/Command/Domain/Abstractions/Identities/GuidIdentity.cs
@@ -6,24 +6,25 @@ public interface IIdentifier;
 
 public abstract record GuidIdentifier : IIdentifier
 {
-    public Guid Value { get; init; }
-    
-    protected GuidIdentifier()
+    private readonly Guid _value;
+
+    protected GuidIdentifier(Guid value) => Value = value;
+    protected GuidIdentifier() => Value = Guid.NewGuid();
+    protected GuidIdentifier(string value) => InvalidIdentifier.ThrowIf(!Guid.TryParse(value, out _value));
+
+    private Guid Value
     {
-        Value = Guid.NewGuid();
+        get => _value;
+        init => _value = value;
     }
 
-    protected GuidIdentifier(string value)
-    {
-        InvalidIdentifier.ThrowIf(
-            Guid.TryParse(value, out var result) is false);
-
-        Value = result;
-    }
+    public static T New<T>(Guid value) where T : GuidIdentifier, new() => new() { Value = value };
 
     public static implicit operator string(GuidIdentifier id) => id.Value.ToString();
     public static implicit operator Guid(GuidIdentifier id) => id.Value;
+
     public static bool operator ==(GuidIdentifier id, string value) => id.Value.CompareTo(value) is 0;
     public static bool operator !=(GuidIdentifier id, string value) => id.Value.CompareTo(value) is not 0;
+
     public override string ToString() => Value.ToString();
 }
