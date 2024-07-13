2ab8dd680a2d0887bfc3536209ccc21658433f2f
Antônio Falcão Jr <arfj@edu.univali.br>
2024-07-13 13:26:55 -0300

    Evolving Exceptions design

--- src/Services/Notification/Command/Domain/Exceptions.cs
+++ src/Services/Notification/Command/Domain/Exceptions.cs
@@ -5,10 +5,7 @@ namespace Domain;
 public static class Exceptions
 {
     public class InvalidIdentifier() : DomainException<InvalidIdentifier>("Invalid identifier.");
-    public class AggregateNotFound() : DomainException<AggregateNotFound>("Aggregate not found.");
-    public class ProductInventoryNotEnough() : DomainException<ProductInventoryNotEnough>("Product inventory is not enough");
     public class AggregateIsDeleted() : DomainException<AggregateIsDeleted>("Aggregate is deleted.");
-    public class InvalidQuantity() : DomainException<InvalidQuantity>("Invalid quantity.");
     public class VersionMustBePositive() : DomainException<VersionMustBePositive>("Version must be a positive number.");
     public class VersionFormatException() : DomainException<VersionFormatException>("Version must be a positive number.");
 }
