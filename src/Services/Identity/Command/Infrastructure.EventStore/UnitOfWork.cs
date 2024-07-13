dc2e08e7865a356e7273e121131b54a9771c7ee4
Antônio Falcão Jr <arfj@edu.univali.br>
2024-07-13 13:55:49 -0300

    Evolving UnitOfWork design

--- src/Services/Identity/Command/Infrastructure.EventStore/UnitOfWork.cs
+++ src/Services/Identity/Command/Infrastructure.EventStore/UnitOfWork.cs
@@ -8,13 +8,13 @@ public class UnitOfWork(DbContext dbContext) : IUnitOfWork
 {
     private readonly DatabaseFacade _database = dbContext.Database;
 
-    public Task ExecuteAsync(Func<CancellationToken, Task> operationAsync, CancellationToken cancellationToken)
-        => _database.CreateExecutionStrategy().ExecuteAsync(ct => ExecuteTransactionAsync(operationAsync, ct), cancellationToken);
+    public Task ExecuteAsync(Func<CancellationToken, Task> operationAsync, CancellationToken token)
+        => _database.CreateExecutionStrategy().ExecuteAsync(ct => ExecuteTransactionAsync(operationAsync, ct), token);
 
-    private async Task ExecuteTransactionAsync(Func<CancellationToken, Task> operationAsync, CancellationToken cancellationToken)
+    private async Task ExecuteTransactionAsync(Func<CancellationToken, Task> operationAsync, CancellationToken token)
     {
-        await using var transaction = await _database.BeginTransactionAsync(cancellationToken);
-        await operationAsync(cancellationToken);
-        await transaction.CommitAsync(cancellationToken);
+        await using var transaction = await _database.BeginTransactionAsync(token);
+        await operationAsync(token);
+        await transaction.CommitAsync(token);
     }
 }
