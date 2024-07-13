69d6bb23e14f2c729c9f5000a13f9a8f801fa3ea
Antônio Falcão Jr <arfj@edu.univali.br>
2024-07-13 12:28:47 -0300

    Evolving AggregateRoots design

--- src/Services/Notification/Command/Domain/Abstractions/Aggregates/AggregateRoot.cs
+++ src/Services/Notification/Command/Domain/Abstractions/Aggregates/AggregateRoot.cs
@@ -1,31 +1,24 @@
 ﻿using Contracts.Abstractions.Messages;
 using Domain.Abstractions.Entities;
 using Domain.Abstractions.Identities;
+using static Domain.Exceptions;
 using Version = Domain.ValueObjects.Version;
 
 namespace Domain.Abstractions.Aggregates;
 
-public abstract class AggregateRoot<TId> : Entity<TId>, IAggregateRoot<TId>
+public abstract class AggregateRoot<TId> : Entity<TId>, IAggregateRoot<TId> 
     where TId : IIdentifier, new()
 {
     private readonly Queue<IDomainEvent> _events = new();
-    public Version Version { get; private set; } = Version.Zero;
+    public Version Version { get; protected set; } = Version.Zero;
 
-    public void LoadFromHistory(IEnumerable<IDomainEvent> events)
-    {
-        foreach (var @event in events)
-        {
-            ApplyEvent(@event);
-            Version = (Version)@event.Version;
-        }
-    }
-
-    public bool TryDequeueEvent(out IDomainEvent? @event) => _events.TryDequeue(out @event);
+    public void LoadFromStream(List<IDomainEvent> events) => events.ForEach(ApplyEvent);
+    public bool TryDequeueEvent(out IDomainEvent @event) => _events.TryDequeue(out @event!);
     private void EnqueueEvent(IDomainEvent @event) => _events.Enqueue(@event);
 
     protected void RaiseEvent(IDomainEvent @event)
     {
-        Version = Version.Next;
+        AggregateIsDeleted.ThrowIf(IsDeleted);
         ApplyEvent(@event);
         EnqueueEvent(@event);
     }
