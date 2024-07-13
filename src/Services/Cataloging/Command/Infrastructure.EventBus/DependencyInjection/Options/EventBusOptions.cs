e162f08db0916d061b060970ac124e6f471df643
Antônio Falcão Jr <arfj@edu.univali.br>
2024-07-13 13:49:01 -0300

    Evolving EventBusOptions validations

--- src/Services/Cataloging/Command/Infrastructure.EventBus/DependencyInjection/Options/EventBusOptions.cs
+++ src/Services/Cataloging/Command/Infrastructure.EventBus/DependencyInjection/Options/EventBusOptions.cs
@@ -7,6 +7,6 @@ public record EventBusOptions
     [Required] public required string ConnectionName { get; init; }
     [Required] public required Uri ConnectionString { get; init; }
     [Required, Range(1, 10)] public int RetryLimit { get; init; }
-    [Required, Timestamp] public TimeSpan InitialInterval { get; init; }
-    [Required, Timestamp] public TimeSpan IntervalIncrement { get; init; }
+    [Required, Range(typeof(TimeSpan), "00:00:01", "00:00:30")] public TimeSpan InitialInterval { get; init; }
+    [Required, Range(typeof(TimeSpan), "00:00:01", "00:00:30")] public TimeSpan IntervalIncrement { get; init; }
 }
