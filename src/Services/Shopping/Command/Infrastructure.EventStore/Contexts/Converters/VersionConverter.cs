061f55d2169794f5bded8adec0f782ffb4f8587d
Antônio Falcão Jr <arfj@edu.univali.br>
2024-07-13 14:15:59 -0300

    Removing unnecessary converters

--- src/Services/Shopping/Command/Infrastructure.EventStore/Contexts/Converters/VersionConverter.cs
+++
@@ -1,7 +0,0 @@
-﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using Version = Domain.ValueObjects.Version;
-
-namespace Infrastructure.EventStore.Contexts.Converters;
-
-public class VersionConverter()
-    : ValueConverter<Version, uint>(version => version, number => Version.Number(number));
